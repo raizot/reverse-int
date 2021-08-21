5516e501e6b4e10e12dad4b548758be908b05517
f <urka94@bk.ru>
2021-08-21 22:03:21 +0300

    feat: add reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function reverse (n) {
-  
+  let newArr = [];
+  let arrDigit = String(n).split('');
+  console.log(arrDigit);
+  if(arrDigit[0] == '-'){
+    for (let i = arrDigit.length - 1; i > 0; i--) {
+        newArr.push(arrDigit[i]);
+    }
+  } else{
+      for (let i = arrDigit.length - 1; i > -1; i--) {
+          newArr.push(arrDigit[i]);
+      }
+  }
+
+  return newArr.join('');
 }
